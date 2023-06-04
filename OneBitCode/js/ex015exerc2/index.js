4c9c9bbb45cceacd514cbf612b38ab77a59dbe1b
renanguerra0 <renanguerradev@gmail.com>
2023-06-04 10:26:58 -0300

    Update index.js

--- OneBitCode/js/ex015exerc2/index.js
+++ OneBitCode/js/ex015exerc2/index.js
@@ -1,8 +1,16 @@
 
-let num1 = window.prompt("Insira o primeiro número")
-let num2 = window.prompt("Insira o segundo número")
+const num1 = window.prompt("Insira o primeiro número")
+const num2 = window.prompt("Insira o segundo número")
 
-console.log(num1 * num2)
-console.log(num1 / num2)
-console.log(parseFloat(num1) + parseFloat(num2))
-console.log(num1 - num2)
+const soma = num1 + num2
+const subtracao = num1 - num2
+const multiplicacao = num1 * num2
+const divisao = num1 / num2
+
+alert(
+    "Resultados:\n" +
+    "\nSoma: " + soma +
+    "\nSubtração: " + subtracao +
+    "\nMultiplicação: " + multiplicacao +
+    "\nDivisão: " + divisao
+)
