ceed9be947ef7cc529b10bf01dc02951fb929b68
renanguerra0 <renanguerradev@gmail.com>
2023-06-11 21:54:42 -0300

    exercício

--- OneBitCode/js/ex019/controle.js
+++ OneBitCode/js/ex019/controle.js
@@ -1,8 +1,12 @@
 
+
+//Utilizando o WHILE
+/*
 let valorInicial = parseFloat(prompt("Informe a quantidade inicial: "))
 let escolhas = 0
 let adicionarValor = 0
 let reduzirValor = 0
+
 
 while (escolhas !== 3){
     
@@ -22,4 +26,38 @@ while (escolhas !== 3){
             alert("PROGRAMA ENCERRADO!")
     }
 }
+*/
 
+
+
+//Utilizando o DO WHILE
+
+let saldo = parseFloat(prompt("Informe a quantioadde de dinheiro inicial: "))
+let opcao = ""
+
+do {
+    opcao = prompt(
+        "Saldo disponível: R$ " + saldo + "\n1. Adicionar dinheiro " +
+        "\n2. Remover dinheiro" +
+        "\n3. Sair"
+    )
+
+    switch (opcao) {
+        case "1":
+        saldo += parseFloat(prompt("Informe o valor a ser adicionado:"))
+        break
+
+        case "2":
+        saldo -= parseFloat(prompt("Informe o valor a ser adicionado:"))
+        break
+
+        case "3":
+        alert("Saindo...")
+        break
+
+        default:
+        alert("Opção inválida.")
+        break
+    }
+
+} while (opcao !== "3") {}
--- OneBitCode/js/ex019/menu.js
+++ OneBitCode/js/ex019/menu.js
@@ -1,7 +1,7 @@
 
-
+//Utilizando o WHILE
+/*
 let escolhas = 0
-
 
 while (escolhas !== 5) {
     escolhas = parseFloat(prompt("Escolha uma das alternativas abaixo: \n1.Opção \n2.Opção \n3.Opção \n4.Opção \n5.Encerrar"))
@@ -24,3 +24,32 @@ while (escolhas !== 5) {
         break
     }
 }
+*/
+
+
+//Utilizando o DO WHILE
+let escolhas = 0
+
+do {
+    escolhas = parseFloat(prompt("Escolha uma das alternativas abaixo: \n1.Opção \n2.Opção \n3.Opção \n4.Opção \n5.Encerrar"))
+
+    switch (escolhas){
+        case 1:
+            alert("Opção 1 escolhida.")
+        break
+        case 2:
+            alert("Opção 2 escolhida.")
+        break
+        case 3:
+            alert("Opção 3 escolhida.")
+        break
+        case 4:
+            alert("Opção 4 escolhida.")
+        break
+        default:
+            alert("Opção 5 escolhida. Encerrando...")
+        break
+    }
+
+}
+while (escolhas !== 5) {}
