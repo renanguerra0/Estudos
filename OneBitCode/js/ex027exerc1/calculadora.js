5f980a0968e8ccbaa6cd69eead09251e57e69a60
renanguerra0 <renanguerradev@gmail.com>
2023-06-25 21:16:33 -0300

    Exercicio

---
+++ OneBitCode/js/ex027exerc1/calculadora.js
@@ -0,0 +1,88 @@
+opcao = "";
+
+do {
+  opcao = prompt(
+    "Escolha uma das opções abaixo:\n" +
+      "1. Área do Triângulo\n" +
+      "2. Área do retângulo\n" +
+      "3. Área do quadrado\n" +
+      "4. Área do trapézio\n" +
+      "5. Área do círculo\n" +
+      "6. Sair"
+  );
+
+  switch (opcao) {
+    case "1":
+      base = prompt("Informe a base");
+      altura = prompt("Informe a altura");
+      function areaTriangulo(base, altura) {
+        return (base * altura) / 2;
+      }
+      resultado = areaTriangulo(base, altura);
+      alert(
+        "Um triângulo com a base " +
+          base +
+          " e a altura " +
+          altura +
+          " possui uma área de " +
+          resultado
+      );
+      break;
+    case "2":
+      base = prompt("Informe a base");
+      altura = prompt("Informe a altura");
+      function areaRetangulo(base, altura) {
+        return base * altura;
+      }
+      resultado = areaRetangulo(base, altura);
+      alert(
+        "Um retângulo com a base " +
+          base +
+          " e a altura " +
+          altura +
+          " possui uma área de " +
+          resultado
+      );
+      break;
+
+    case "3":
+      lado = prompt("Informe o lado");
+      function areaQuadrado(lado) {
+        return Math.pow(lado, 2);
+      }
+      resultado = areaQuadrado(lado);
+      alert(
+        "Um quadrado com o lado " + lado + " possui uma área de " + resultado
+      );
+      break;
+
+    case "4":
+      baseMaior = parseFloat(prompt("Informe a base maior:"));
+      baseMenor = parseFloat(prompt("Informe a base menor:"));
+      altura = prompt("Informe a altura:");
+      function areaTrapezio(baseMaior, baseMenor, altura) {
+        return ((baseMaior + baseMenor) * altura) / 2;
+      }
+      resultado = areaTrapezio(baseMaior, baseMenor, altura);
+      alert("Um trapezio com a base maior de " + baseMaior + ", base menor " + baseMenor + " e altura de " + altura + " possui uma área de " + resultado);
+      break;
+
+    case "5":
+      raio = parseFloat(prompt("Informe o raio"))
+      function areaCirculo(raio){
+        return 3.14 * (Math.pow(raio, 2))
+      }
+      resultado = areaCirculo(raio)
+      alert("O circulo com raio de " + raio + " possui uma área de " + resultado)
+      break
+
+    case "6":
+      alert("Encerrando...")
+      break
+
+    default:
+      alert("Opção inválida, tente novamente")
+      break
+
+  }
+} while (opcao != "6"){}
