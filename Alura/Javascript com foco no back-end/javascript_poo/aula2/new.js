e0bb93c8c8c67e70bc20c58ecebd533589987f0f
renanguerra0 <renanguerradev@gmail.com>
2024-01-14 23:29:08 -0300

    v3

--- Alura/Javascript com foco no back-end/javascript_poo/aula2/new.js
+++ Alura/Javascript com foco no back-end/javascript_poo/aula2/new.js
@@ -1,21 +1,26 @@
-function User(nome, email){
-    this.nome =  nome
+
+const user = {
+  init: function(nome, email) {
+    this.nome = nome
     this.email = email
+  },
 
-    this.exibirInfos = function(){
-        return `${this.nome}, ${this.email}`
-    }
-};
+  exibirInfos: function() {
+    return this.nome
+  }
+}
 
-// const novoUser = new User('Juliana', 'j@j.com')
-// console.log(novoUser.exibirInfos())
-
-function Admin(role){
-    User.call(this, 'Juliana', 'j@j.com')
-    this.role = role || 'estudante'
-};
-
-Admin.prototype = Object.create(User.prototype)
-const novoUser = new Admin('admin')
+const novoUser = Object.create(user)
+// console.log(novoUser.exibirInfos('Juliana'))
+// console.log(user.isPrototypeOf(novoUser))
+novoUser.init('Juliana', 'j@j.com')
 console.log(novoUser.exibirInfos())
-console.log(novoUser.role)
+
+
+
+
+
+
+
+
+
--- Alura/Javascript com foco no back-end/javascript_poo/aula2/objetoLiteral.js
+++ Alura/Javascript com foco no back-end/javascript_poo/aula2/objetoLiteral.js
@@ -1,28 +1,31 @@
-//Herança de protótipo
-
 const user = {
-    nome: "Juliana",
-    email: "j@j.com",
-    nascimento: "2021/01/01",
-    role: "estudante",
-    ativo: true,
-    exibirInfos: function(){
-        console.log(this.nome, this.email)
-    }
+  nome: "Juliana",
+  email: "j@j.com",
+  nascimento: "2021/01/01",
+  role: "estudante",
+  ativo: true,
+  exibirInfos: function() {
+    console.log(this.nome, this.email)
+  }
 }
 
 const admin = {
-    nome: "Mariana",
-    email: "m@m.com",
-    role: "admin",
-    criarCurso(){
-        console.log("Curso criado!")
-    }
+  nome: "Mariana",
+  email: "m@m.com",
+  role: "admin",
+  criarCurso() {
+    console.log('curso criado!')
+  }
 }
-
 
 //Nesse método precisamos passar 2 parâmetros: 
 //1)Objeto que vai herdar as propriedades 2)Objeto que vai ceder as propriedades
 Object.setPrototypeOf(admin, user)
 admin.criarCurso()
 admin.exibirInfos()
+
+
+
+
+
+
